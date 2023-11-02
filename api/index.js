821ebf202c5cc2a30102b4985551ecf3b96b812e
AliAkramkk <akramkorakkottil@gmail.com>
2023-11-02 18:52:48 +0530

    database modified

--- api/index.js
+++ api/index.js
@@ -1,17 +1,27 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-// initialise the env
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
-mongoose.connect(process.env.MONGO).then(() => {
-  console.log('connected to Mongodb');
-}).catch((err) => {
-  console.log(err);
+mongoose.connect(process.env.MONGO, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("Database Connected")
+}).catch((error) => {
+  console.log(error.message)
 })
+
 
 const app = express();
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
+});
+
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'API is working'
+  })
 })
